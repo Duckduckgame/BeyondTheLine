e77223e89e9ef4e04b342c2af32b87f308343295
Duckduckgame <gabriel.h.kenny@gmail.com>
2019-10-24 16:23:41 +0100

    added track 01 and 00

---
+++ Beyond The Line/Assets/tutorialController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class tutorialController : MonoBehaviour
+{
+    [HideInInspector]
+    bool showR2;
+    [HideInInspector]
+    bool showL3;
+    [HideInInspector]
+    bool showSticking;
+    [HideInInspector]
+    bool showStrafe;
+
+    [SerializeField]
+    CanvasGroup R2Canvas;
+    [SerializeField]
+    CanvasGroup L3Canvas;
+    [SerializeField]
+    CanvasGroup stickCanvas;
+    [SerializeField]
+    CanvasGroup strafeCanvas;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void setR2Canvas()
+    {
+
+    }
+    void setL3Canvas()
+    {
+
+    }
+    void setStickCanvas()
+    {
+
+    }
+    void setStrafeCanvas()
+    {
+
+    }
+}
