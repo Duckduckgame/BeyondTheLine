8a3f48c3810e82cc776017cd33a349acb02c03eb
Duckduckgame <gabriel.h.kenny@gmail.com>
2019-10-25 14:05:42 +0100

    finished tutorial lap

---
+++ Beyond The Line/Assets/Scripts/tutorial/TutorialcolTrigger.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialcolTrigger : MonoBehaviour
+{
+    enum phaseTrigger {R2, L3, Strafe, Stick }
+
+    [SerializeField]
+    phaseTrigger crntTrigger;
+
+    tutorialController tutorialController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tutorialController = FindObjectOfType<tutorialController>();
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (crntTrigger == phaseTrigger.R2)
+        {
+            tutorialController.setR2Canvas();
+        }
+        if (crntTrigger == phaseTrigger.L3)
+        {
+            tutorialController.setL3Canvas();
+        }
+        if (crntTrigger == phaseTrigger.Strafe)
+        {
+            tutorialController.setStrafeCanvas();
+        }
+        if (crntTrigger == phaseTrigger.Stick)
+        {
+            tutorialController.setStickCanvas();
+        }
+        Destroy(this);
+    }
+}
---
+++ Beyond The Line/Assets/Scripts/tutorial/tutorialController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tutorialController : MonoBehaviour
+{
+    [HideInInspector]
+    public bool showR2;
+    [HideInInspector]
+    public bool showL3;
+    [HideInInspector]
+    public bool showSticking;
+    [HideInInspector]
+    public bool showStrafe;
+
+    [SerializeField]
+    CanvasGroup R2Canvas;
+    [SerializeField]
+    CanvasGroup L3Canvas;
+    [SerializeField]
+    CanvasGroup stickCanvas;
+    [SerializeField]
+    CanvasGroup strafeCanvas;
+
+    CanvasGroup crntCanvas;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(crntCanvas != null && Input.GetButtonDown("Jump"))
+        {
+            crntCanvas.alpha = 0;
+            Time.timeScale = 1;
+        }
+    }
+
+    public void setR2Canvas()
+    {
+        R2Canvas.alpha = 1;
+        crntCanvas = R2Canvas;
+        Time.timeScale = 0.0001f;
+    }
+    public void setL3Canvas()
+    {
+        L3Canvas.alpha = 1;
+        crntCanvas = L3Canvas;
+        Time.timeScale = 0.0001f;
+    }
+    public void setStickCanvas()
+    {
+        stickCanvas.alpha = 1;
+        crntCanvas = stickCanvas;
+        Time.timeScale = 0.0001f;
+    }
+    public void setStrafeCanvas()
+    {
+        strafeCanvas.alpha = 1;
+        crntCanvas = strafeCanvas;
+        Time.timeScale = 0.0001f;
+    }
+}
